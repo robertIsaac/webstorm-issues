84f2f8ab0475b314e6ba2bcf2573edeb7f22f994
Robert Isaac <robert.isaac@sap.com>
2025-02-13 20:11:06 +0200

    reproduce issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {NgxEchartsDirective} from 'ngx-echarts';
+import {EChartsOption} from 'echarts';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NgxEchartsDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'webstorm-issues';
+  readonly chartOptions: EChartsOption = {};
 }
